6f7d881d409bdbc34bfad1cf57268608d271f8f7
takayuki tanaka <takayuki.1981.0317@gmail.com>
2019-02-23 16:39:40 +0900

    add exit shell command

--- src/entry.rs
+++ src/entry.rs
@@ -10,14 +10,28 @@ pub fn start() {
 // シェル
 fn shell() {
     uart::send("> ");
+    let mut prev = 0;
+    let mut len = 0;
     loop {
         let c = uart::recv();
-        if c == 0x0a || c == 0x0d {
+        if c == 0x0d {
             uart::send("\n");
+
+            // 前の入力がqであれば抜ける
+            if len == 1 && prev == 0x71 {
+                return;
+            }
             uart::send("> ");
+
+            // コマンド初期化
+            len = 0;
         }
         else {
             uart::send_byte(&[c]);
+
+            // 前の入力を保存
+            prev = c;
+            len += 1;
         }
     }
 }
